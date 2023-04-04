a8dbe1aacc95c792b6f64bd596bdf0cbeb533623
Prince <prinsamsaman@gmail.com>
2023-04-04 15:27:10 +0800

    first commit

---
+++ single.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+<div class="post">
+    <div class="container">
+        <div class="postWrapper">
+            <h1><?php the_title(); ?></h1>
+            <?php echo get_the_post_thumbnail(); ?>
+            <p>
+                <small><?php echo get_the_date('M, d, Y'); ?></small>
+            </p>
+            <p><?php the_excerpt(); ?></p>
+            <p><?php the_content(); ?></p>
+
+        </div>
+
+    </div>
+</div>
+<?php get_footer(); ?>
